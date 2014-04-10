63f62b535142fd4da61e32e0d375ddd92da97ee1
amikryukov <amikryukov@griddynamics.com>
2014-04-10 15:40:20 +0400

    init commit color is int field

---
+++ src/main/java/model/series/Curve.java
@@ -0,0 +1,44 @@
+package model.series;
+
+import java.util.List;
+
+public class Curve {
+
+    private String title;
+    // may be here should be something else - not int;
+    private int color;
+    private List<Point> points;
+
+    public Curve() {
+    }
+
+    public Curve(String title, int color, List<Point> points) {
+        this.title = title;
+        this.color = color;
+        this.points = points;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
+}
